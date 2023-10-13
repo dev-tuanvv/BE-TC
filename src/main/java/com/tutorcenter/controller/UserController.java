1a657cbbf0a21af0f414c6a68a81219f2827e5bd
Hoang Do <dohuyhoang1120@gmail.com>
2023-10-14 00:59:21 +0700

    add new

--- src/main/java/com/tutorcenter/controller/UserController.java
+++ src/main/java/com/tutorcenter/controller/UserController.java
@@ -2,13 +2,22 @@ package com.tutorcenter.controller;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutorcenter.model.CustomUserDetails;
 import com.tutorcenter.model.User;
 import com.tutorcenter.service.UserService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,4 +31,11 @@ public class UserController {
 
         return userService.findAll();
     }
+
+    // @PostMapping("/register")
+    // public ResponseEntity<?> register(@RequestBody String email, @RequestBody
+    // String password) {
+
+    // new ResponseEntity<>(null, null, HttpStatus.OK);
+    // }
 }
--- src/main/java/com/tutorcenter/repository/UserRepository.java
+++ src/main/java/com/tutorcenter/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.tutorcenter.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.tutorcenter.model.User;
@@ -8,4 +11,7 @@ import com.tutorcenter.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    User getUserByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
