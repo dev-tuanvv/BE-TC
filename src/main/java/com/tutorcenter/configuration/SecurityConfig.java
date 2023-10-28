d3fdd17fba32dc524edbd8c165b02a85e0a4ef02
dev-tuanvv <tuanvv.dev@gmail.com>
2023-10-28 14:26:46 +0700

    Update SecurityConfig.java

--- src/main/java/com/tutorcenter/configuration/SecurityConfig.java
+++ src/main/java/com/tutorcenter/configuration/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
-                                .cors(cors -> cors.disable())
+                               
                                 .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL)
                                                 .permitAll()
