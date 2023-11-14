5113c03e84e698c8d744c6aaa95af18c90726874
TuanVo <tuan.vo@alttekglobal.com>
2023-11-14 22:49:35 +0700

    GetParentById

--- src/main/java/com/tutorcenter/controller/ParentController.java
+++ src/main/java/com/tutorcenter/controller/ParentController.java
@@ -3,11 +3,15 @@ package com.tutorcenter.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutorcenter.dto.ApiResponseDto;
+import com.tutorcenter.dto.parent.ParentDetailDto;
+import com.tutorcenter.dto.request.RequestResDto;
 import com.tutorcenter.model.Parent;
 import com.tutorcenter.service.ParentService;
 
@@ -24,8 +28,13 @@ public class ParentController {
     }
 
     @GetMapping("/{id}")
-    public Parent getParentById(@PathVariable int id) {
-        return parentService.getParentById(id).orElseThrow();
+    public ApiResponseDto<ParentDetailDto> getParentById(@PathVariable int id) {
+        Parent model = parentService.getParentById(id).orElse(null);
+        if (model == null) {
+            return ApiResponseDto.<ParentDetailDto>builder().responseCode("404").build();
+        }
+        ParentDetailDto dto = new ParentDetailDto();
+        dto.fromParent(model);
+        return ApiResponseDto.<ParentDetailDto>builder().data(dto).build();
     }
-
 }
---
+++ src/main/java/com/tutorcenter/dto/parent/ParentDetailDto.java
@@ -0,0 +1,24 @@
+package com.tutorcenter.dto.parent;
+
+import com.tutorcenter.model.Parent;
+
+import lombok.Data;
+
+@Data
+public class ParentDetailDto {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String address;
+    private String districtName;
+    private String provinceName;
+
+   public void fromParent(Parent parent) {
+        fullName = parent.getFullname();
+        email = parent.getEmail();
+        phone = parent.getPhone();
+        address = parent.getAddress();
+        districtName = parent.getDistrict().getName();
+        provinceName = parent.getDistrict().getProvince().getName();
+    }
+}
