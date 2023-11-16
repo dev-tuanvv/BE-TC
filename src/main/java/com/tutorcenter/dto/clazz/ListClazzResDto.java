969de82b5ee63a52c2e82263dbc48b224b83610f
hoangtiot <dohuyhoang1120@gmail.com>
2023-11-16 07:30:07 +0700

    add slots + slotsLength to list clazz res dto

--- src/main/java/com/tutorcenter/dto/clazz/ListClazzResDto.java
+++ src/main/java/com/tutorcenter/dto/clazz/ListClazzResDto.java
@@ -12,6 +12,8 @@ public class ListClazzResDto {
     private int id;
     private int requestId;
     private List<SubjectLevelResDto> subjects;
+    private int slots;
+    private int slotsLength;
     private String tutorLevel;
     private String address;
     private String districtName;
@@ -23,6 +25,8 @@ public class ListClazzResDto {
     public void fromClazz(Clazz clazz) {
         this.id = clazz.getId();
         this.requestId = clazz.getRequest().getId();
+        this.slots = clazz.getRequest().getSlots();
+        this.slotsLength = clazz.getRequest().getSlotsLength();
         this.tutorLevel = clazz.getRequest().getTutorLevel();
         this.address = clazz.getRequest().getAddress();
         this.districtName = clazz.getRequest().getDistrict().getName();
