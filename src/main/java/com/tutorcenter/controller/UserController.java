1fc8113b684e0da2d2de02cb8cdca80a53935189
TuanVo <tuan.vo@alttekglobal.com>
2023-11-26 01:13:20 +0700

    upload file

--- src/main/java/com/tutorcenter/controller/UserController.java
+++ src/main/java/com/tutorcenter/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.tutorcenter.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tutorcenter.common.Common;
 import com.tutorcenter.constant.Role;
@@ -16,6 +27,7 @@ import com.tutorcenter.service.UserService;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads/";
 
     @Autowired
     private UserService userService;
@@ -36,6 +48,22 @@ public class UserController {
         } catch (Exception e) {
             return ApiResponseDto.<AuthProfileDto>builder().responseCode("500").message(e.getMessage()).build();
         }
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+
+        Path pathFolder = Paths.get(UPLOAD_DIRECTORY);
+
+        if (!Files.exists(pathFolder)) {
+            Files.createDirectories(pathFolder);
+        }
+
+        Path pathFile = pathFolder.resolve(UUID.randomUUID() + file.getOriginalFilename());
+
+        Files.createFile(pathFile);
+        Files.write(pathFile, file.getBytes());
+        return new ResponseEntity<>("Image uploaded successfully!", HttpStatus.OK);
     }
 
 }
