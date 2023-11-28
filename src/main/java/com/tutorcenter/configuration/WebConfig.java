9d373fc8952d63150c2626ef1d80d8ca1eda5c16
TuanVo <tuan.vo@alttekglobal.com>
2023-11-28 23:41:10 +0700

    Update WebConfig.java

--- src/main/java/com/tutorcenter/configuration/WebConfig.java
+++ src/main/java/com/tutorcenter/configuration/WebConfig.java
@@ -2,11 +2,17 @@ package com.tutorcenter.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("*");
