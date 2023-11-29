9171d917554c2c075e1f6b0c2606434507b8c935
hoangtiot <dohuyhoang1120@gmail.com>
2023-11-29 21:59:05 +0700

    fix TutorResDto

--- src/main/java/com/tutorcenter/dto/tutor/TutorResDto.java
+++ src/main/java/com/tutorcenter/dto/tutor/TutorResDto.java
@@ -14,22 +14,28 @@ public class TutorResDto {
     private String tutorName;
     private List<SubjectLevelResDto> subjects;
     private String gender;
+    private String email;
+    private String phone;
     private String districtName;
     private String provinceName;
     private String university;
     private String major;
     private String imgCert;
     private String imgAvatar;
+    private int status;
 
     public void fromTutor(Tutor tutor) {
         this.id = tutor.getId();
         this.tutorName = tutor.getFullname();
         this.gender = tutor.getGender();
+        this.email = tutor.getEmail();
+        this.phone = tutor.getPhone();
         this.districtName = tutor.getDistrict().getName();
         this.provinceName = tutor.getDistrict().getProvince().getName();
         this.university = tutor.getUniversity();
         this.major = tutor.getMajor();
         this.imgCert = tutor.getImgCertificate();
         this.imgAvatar = tutor.getImgAvatar();
+        this.status = tutor.getStatus();
     }
 }
