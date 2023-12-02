534af617c7af1a30451c28038651bc9eea000c90
hoangtiot <dohuyhoang1120@gmail.com>
2023-12-02 13:49:42 +0700

    add noti register

--- src/main/java/com/tutorcenter/controller/BlogController.java
+++ src/main/java/com/tutorcenter/controller/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,7 +59,7 @@ public class BlogController {
     }
 
     @PreAuthorize("hasAnyAuthority('manager:create')")
-    @PutMapping("/create")
+    @PostMapping("/create")
     public ApiResponseDto<Integer> createBlog(@RequestParam BlogReqDto dto) {
         try {
             Blog blog = new Blog();
